95fb7308f7ac3651fcae56bbc4729150107f1bbf
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2024-03-06 15:21:18 +0100

    delete ItemFilter.cs

--- PujcovadloServer/Areas/Admin/Business/Filters/ItemFilter.cs
+++
@@ -1,6 +0,0 @@
-namespace PujcovadloServer.Areas.Admin.Business.Filters;
-
-public class ItemFilter : PujcovadloServer.Business.Filters.ItemFilter
-{
-    public override int PageSize { get; set; } = 10;
-}
